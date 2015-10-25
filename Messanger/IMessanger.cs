601e0fea71021057db626617c2fe8a026f1a767c
MM <mmpyro@gmail.com>
2015-10-25 12:44:29 +0100

    added messanger draft

---
+++ Messanger/IMessanger.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Messanger
+{
+    public interface IMessanger
+    {
+        void Register(Type type, Action<object> action);
+        void Send<T>(T message);
+    }
+}
---
+++ Messanger/Messanger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Messanger
+{
+    public class Messanger : IMessanger
+    {
+        private readonly Dictionary<Type,List<Action<object>>> _dict;
+        private static Messanger _instance;
+
+        private Messanger()
+        {
+            _dict = new Dictionary<Type, List<Action<object>>>();
+        }
+
+        public static Messanger GetInstance()
+        {
+            return _instance ?? (_instance = new Messanger());
+        }
+
+        public void Register(Type type, Action<object> action)
+        {
+            List<Action<object>> list = null;
+            if (!_dict.ContainsKey(type))
+            {
+                list = _dict[type] ?? new List<Action<object>>();
+            }
+            list.Add(action);
+        }
+
+        public void Send<T>(T message)
+        {
+            Type messageType = typeof (T);
+            if (_dict.ContainsKey(messageType))
+            {
+                _dict[messageType].ForEach(t => t.Invoke(message));
+            }
+        }
+    }
+}
