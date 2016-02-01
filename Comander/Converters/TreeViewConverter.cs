2a1b30ef8e6d31415b88ab6cdbe4a91acbb2861c
Michal Marszalek <mmpyro@gmail.com>
2016-02-01 21:35:51 +0100

    changes in tree view

---
+++ Comander/Converters/TreeViewConverter.cs
@@ -0,0 +1,29 @@
+﻿using Comander.ViewModel;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Comander.Converters
+{
+    public class TreeViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            File file = value as File;
+            if(file != null)
+            {
+                if (file.IsDir)
+                {
+                    return @"..\Icons\treefolder.png";
+                }
+                return @"..\Icons\treefiles.png";
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
